f252e0526034d80c3ee926f78b7ac56e06012e1d
Jeremy Yiu <utsjeremy@gmail.com>
2017-10-30 21:17:44 +1100

    Added Wifi toggle functionality

--- app/src/main/java/jauts/org/jnabatteryoptimiser/MainActivity.java
+++ app/src/main/java/jauts/org/jnabatteryoptimiser/MainActivity.java
@@ -1,5 +1,7 @@
 package jauts.org.jnabatteryoptimiser;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,7 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-
+    private void toggleWifi(boolean request) {
+        WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        try {
+            boolean wifiEnabled = wifiManager.isWifiEnabled();
+            System.out.println(wifiEnabled);
+            if (request != wifiEnabled) {
+                wifiManager.setWifiEnabled(request);
+            }
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+            System.exit(1);
+        }
     }
 }
