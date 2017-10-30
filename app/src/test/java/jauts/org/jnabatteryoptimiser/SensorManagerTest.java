b093847039c2b2daa97e64393667a1c2dbf6a89f
lkxlaz <lkxlaz@live.com>
2017-10-30 16:26:08 +1100

    imported jar files

---
+++ app/src/test/java/jauts/org/jnabatteryoptimiser/SensorManagerTest.java
@@ -0,0 +1,14 @@
+package jauts.org.jnabatteryoptimiser;
+
+import org.junit.Test;
+
+/**
+ * Created by liangze on 30/10/17.
+ */
+
+public class SensorManagerTest {
+    @Test
+    public void senseDataTest() {
+        
+    }
+}
