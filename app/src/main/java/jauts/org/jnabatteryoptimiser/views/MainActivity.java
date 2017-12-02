27d7183fe96bf4cdd65585fbaf57e35deb268cb4
Jeremy Yiu <utsjeremy@gmail.com>
2017-12-03 04:47:14 +1100

    Fixed tab display after merging with the develop branch

--- app/src/main/java/jauts/org/jnabatteryoptimiser/views/MainActivity.java
+++ app/src/main/java/jauts/org/jnabatteryoptimiser/views/MainActivity.java
@@ -1,23 +1,64 @@
 package jauts.org.jnabatteryoptimiser.views;
 
 import android.Manifest;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
 
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
+import jauts.org.jnabatteryoptimiser.AppsFragment;
+import jauts.org.jnabatteryoptimiser.PagerAdapter;
+import jauts.org.jnabatteryoptimiser.PullSensorsFragment;
+import jauts.org.jnabatteryoptimiser.PushSensorsFragment;
 import jauts.org.jnabatteryoptimiser.R;
+import jauts.org.jnabatteryoptimiser.dummy.DummyContent;
+import jauts.org.jnabatteryoptimiser.tasks.SenseFromAllEnvSensorsTask;
 import jauts.org.jnabatteryoptimiser.tasks.SenseFromAllPullSensorsTask;
 import jauts.org.jnabatteryoptimiser.tasks.SenseFromAllPushSensorsTask;
+import jauts.org.jnabatteryoptimiser.tasks.WekaMlTask;
 
+public class MainActivity extends AppCompatActivity implements PullSensorsFragment.OnListFragmentInteractionListener, PushSensorsFragment.OnListFragmentInteractionListener, AppsFragment.OnListFragmentInteractionListener {
 
-public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Pull Sensors"));
+        tabLayout.addTab(tabLayout.newTab().setText("Push Sensors"));
+        tabLayout.addTab(tabLayout.newTab().setText("Running Apps"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         grantLocation();
         collectSensorData();
@@ -27,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         new RxPermissions(this)
                 .request(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
                 .subscribe(granted -> {
-                        Log.d("LocationGranted", String.valueOf(granted));
+                    Log.d("LocationGranted", String.valueOf(granted));
                 });
 
     }
@@ -38,4 +79,7 @@ public class MainActivity extends AppCompatActivity {
         new SenseFromAllPullSensorsTask(this).execute();
     }
 
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+    }
 }
